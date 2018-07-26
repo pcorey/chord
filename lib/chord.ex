55424a44409f58e6dafde2a5bfa73b46977ed0af
Pete Corey <petecorey@gmail.com>
2018-07-26 08:56:27 -0600

    First commit

---
+++ lib/chord.ex
@@ -0,0 +1,18 @@
+defmodule Chord do
+  @moduledoc """
+  Documentation for Chord.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Chord.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/chord_test.exs
@@ -0,0 +1,8 @@
+defmodule ChordTest do
+  use ExUnit.Case
+  doctest Chord
+
+  test "greets the world" do
+    assert Chord.hello() == :world
+  end
+end
